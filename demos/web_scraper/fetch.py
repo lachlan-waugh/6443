fc62066434cfeb2cf1ef598585df4b11e1da2ada
Lachlan <lachlanwaugh8@gmail.com>
2022-06-09 21:27:30 +1000

    fixed up formatting issue for week02 slides which hid the last few slides.

--- demos/web_scraper/fetch.py
+++ demos/web_scraper/fetch.py
@@ -4,15 +4,6 @@ import sys
 
 proxy = { "https:": "https://127.0.0.1:8080", "http:" : "http://127.0.0.1:8080" }
 
-def usage():
-	print("Usage: ./fetch.py [options] input"
-		+ "\t -r FILE (a file containing urls to check)"
-		+ "\t -f URL")
-
-def recursive(file):
-	for line in open(file).readlines():
-		read_domain(line.strip())
-
 def read_domain(subd):
 	res = requests.get(f'https://{subd}.quoccabank.com', proxies = proxy)
 
@@ -30,13 +21,3 @@ def post_domain(subd)
 		continue
 
 	print(res.text)
-
-if len(sys.argv) == 3:
-	if (sys.argv[1] == "-r"):
-		recursive(sys.argv[2])
-	elif (sys.argv[1] == "-f"):
-		read_domain(sys.argv[2])
-	else:
-		usage()
-else:
-	usage()
