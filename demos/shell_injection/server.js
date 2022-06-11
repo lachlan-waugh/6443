1b3996b281c11872d7818ca9e142ec0f52b3dc8d
Lachlan <lachlanwaugh8@gmail.com>
2022-06-12 04:50:03 +1000

    cleaned up the shell injection demo a bit.

--- demos/shell_injection/server.js
+++ demos/shell_injection/server.js
@@ -4,26 +4,18 @@ const pug = require('pug');
 
 const app = express();
 
-// Listen in on root
 app.get('/', (req, res) => {
   const folder = req.query.folder;
+
   if (folder) {
     // Run the command with the parameter the user gives us
     exec(`ls -l ${folder}`, (error, stdout, _) => {
-      let output = stdout;
-      if (error) {
-        // If there are any errors, show that
-        output = error; 
-      }
-      res.send(
-        pug.renderFile('./pages/index.pug', {output: output, folder: folder})
-      );
+      let output = (error) ? error : stdout;
+      res.send(pug.renderFile('./pages/index.pug', {output: output, folder: folder}));
     });
   } else {
     res.send(pug.renderFile('./pages/index.pug', {}));
   }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${3000}`);
-});
+app.listen(port, () => { console.log(`Example app listening at http://localhost:${3000}`); });
