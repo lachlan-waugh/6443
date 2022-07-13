927724b0315db8142a92a2b572b2bed0cc4ef9bb
Lachlan <lachlanwaugh8@gmail.com>
2022-07-14 01:04:01 +1000

    finished express demo (except its broken lol)

--- demos/client-side-injection/xss-express/src/db.js
+++ demos/client-side-injection/xss-express/src/db.js
@@ -1,9 +1,13 @@
-const posts = [];
+let posts = [];
 
 export const db_push = (content) => {
     posts.push(content);
-}
+};
 
 export const db_pull = () => {
     return posts;
-}
+};
+
+export const db_clear = () => {
+    posts = []
+};
--- demos/client-side-injection/xss-express/src/server.js
+++ demos/client-side-injection/xss-express/src/server.js
@@ -1,22 +1,27 @@
 import express from 'express';
 import fs from 'fs';
-import { db_push, db_pull } from './db.js';
+import { db_push, db_pull, db_clear } from './db.js';
 
 const app = express();
 const port = 3000;
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.end(fs.readFileSync('./static/index.html'))
+app.get('/', (_, res) => {
+    res.end(fs.readFileSync('site/index.html'))
 });
 
-app.get('/blogs', (req, res) => {
+app.get('/comments', (_, res) => {
     res.send(JSON.stringify(db_pull()))
 });
 
-app.post('/blogs', (req, res) => {
-    db_push(req.body.content);
+app.post('/comments', (req, res) => {
+    db_push(req.body.comment);
+    res.send('success');
+});
+
+app.delete('/comments', (_, res) => {
+    db_clear()
     res.send('success');
 });
 
