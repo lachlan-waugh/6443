bf4a3d278b7fb62213de9df7a2571b0eaf2622d0
Lachlan <lachlanwaugh8@gmail.com>
2022-07-18 00:44:43 +1000

    updated csrf demo to split the bad and bank server into separate files.

--- demos/client-side-injection/csrf/src/server.js
+++ demos/client-side-injection/csrf/src/server.js
@@ -52,25 +52,3 @@ app.post('/send', require_login, (req, res, next) => {
 });
 
 app.listen(3000, () => console.log('bank server listening @ localhost:3000'));
-
-/* 
-	>>>
-*/
-
-const bad_app = express();
-
-bad_app.engine('html', exphbr.engine({
-	defaultLayout: 'main',
-	extname: '.html'
-}));
-bad_app.set('view engine', 'html');
-
-bad_app.get('/', (_, res, next) => res.render('evil/index'));
-
-bad_app.get('/clickme', (_, res, next) => res.render('evil/clickme'));
-
-bad_app.get('/post', (_, res, next) => res.render('evil/post'));
-
-bad_app.get('/form', (_, res, next) => res.render('evil/form'));
-
-bad_app.listen(3001, () => console.log('bad server listening @ localhost:3001'));
