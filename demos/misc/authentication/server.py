07d123c131b46de7fda1de79758c2e889e398956
Lachlan <lachlanwaugh8@gmail.com>
2023-02-23 17:57:29 +1100

    updated week02 slides, and auth demo.

--- demos/misc/authentication/server.py
+++ demos/misc/authentication/server.py
@@ -1,33 +1,41 @@
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, make_response, session, jsonify
 from db import auth
 
 app = Flask(__name__)
 app.config['TITLE'] = 'Comments or somethn'
+app.config['SECRET_KEY'] = 'trivial'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     # basic username/password cookies
-    if req.cookies.get('username', False):
+    if request.cookies.get('username', False):
         # I should probably auth but I really cba
-        return f"welcome " + req.cookies.get('username' 'oh no') + ". Man, '" + req.cookies.get('password', 'we broken') + "' is a really strong password, well done!"
+        return f"welcome " + request.cookies.get('username') + ". Man, '" + request.cookies.get('password') + "' is a really strong password, well done!"
     # flask session cookie
-    elif req.cookies.get('session', False):
-        return f"welcome " + 1 + ". Man, '" + 1 + "' is a really strong password, well done!"
+    elif session.get('active', False):
+        return f"welcome " + session['username'] + ". Man, 'admin' is a really strong password, well done!"
 
     return render_template('login.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.json.get('username', None)
-        password = request.json.get('password', None)
-        success = auth(request.args.get('username', None), request.args.get('password', None))
-        return jsonify({ 'data': f'token={}' })
+        j = request.json
+        username = j.get('username', None)
+        password = j.get('password', None)
+
+        if auth(username, password):
+            session['username'] = username
+            return jsonify({ 'data': f'token=asdf', 'success': True })
     else:
-        username = request.args.get('username', None)
-        password = request.args.get('password', None)
-        success = auth(username, password)
-        return jsonify({ 'data': f'username={username};password={password}' })
+        a = request.args
+        username = a.get('username', None)
+        password = a.get('password', None)
+
+        if auth(username, password):
+            return jsonify({ 'user': username, 'pass': password, 'success': True })
+
+    return jsonify({ 'data': 'invalid credentials', 'success': False })
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
