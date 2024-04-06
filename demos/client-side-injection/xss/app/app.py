99ee3ffff95e74c0264f755e3c8e9c373fbd931d
Lachlan <lachlanwaugh8@gmail.com>
2024-04-06 21:49:10 +1100

    Updated style for the xss demo.

--- demos/client-side-injection/xss/app/app.py
+++ demos/client-side-injection/xss/app/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
 import db
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder='static',
+    static_url_path=''
+)
 app.config['TITLE'] = 'Comments or somethn'
 
 
