e071903fcc4e5b3c729f61d5c7f3bee1f295fa37
Lachlan <lachlanwaugh8@gmail.com>
2024-04-07 19:17:12 +1000

    added solution script to helper.js file.

--- demos/client-side/gadgets/app/static/helper.js
+++ demos/client-side/gadgets/app/static/helper.js
@@ -1,5 +1,6 @@
-const melon = () => {
-    const btn = document.querySelector('[data-role=button]');
-    btn.innerHTML = btn.getAttribute('data-text');
-}
-melon();
+(() => {
+  const btn = document.querySelector('[data-role=button]');
+  btn.innerHTML = btn.getAttribute('data-text');
+})();
+
+// <a data-role="button" data-text="<img src=x onerror=alert(1) />"></a>
