9749ef19ad8baf1a86d540e715421667f4a21843
Lachlan <lachlanwaugh8@gmail.com>
2024-04-07 16:02:28 +1000

    Removed flask_talisman usage from the gadgets demo, just set the headers myself.

--- demos/client-side/gadgets/app/app.py
+++ demos/client-side/gadgets/app/app.py
@@ -1,27 +1,23 @@
 from flask import Flask, render_template, request, make_response
-from flask_talisman import Talisman
+from base64 import b64encode
+from secrets import token_bytes
 
 app = Flask(__name__, static_url_path='')
 app.config['TITLE'] = 'Comments or somethn'
 
-csp = {
-    "object-src": "'none'",
-    "base-uri": "'none'",
-    "script-src": "'strict-dynamic' 'unsafe-eval' cdnjs.cloudflare.com"
-}
-Talisman(
-    app,
-    content_security_policy=csp,
-    content_security_policy_nonce_in=["script-src"],
-    force_https=False,
-    strict_transport_security=False,
-)
-
 
 @app.route('/', methods=['GET'])
 def home():
-    return make_response(render_template('index.html', query=request.args.get('q')))
+    nonce = b64encode(token_bytes(16)).decode()
+    res = make_response(
+        render_template('index.html', query=request.args.get('q'))
+    )
+    res.headers.set(
+        'Content-Security-Policy',
+        f"object-src 'none' base-uri 'none' script-src 'nonce-{nonce}' 'strict-dynamic' 'unsafe-eval' 'cdnjs.cloudflare.com'"
+    )
+    return res
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=7050)
+    app.run(host='0.0.0.0', port=7050)
