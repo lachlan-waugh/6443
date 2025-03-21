f6d6c8a680997d78ad0ebe7fb4648ff28c39760e
Lachlan <lachlanwaugh8@gmail.com>
2025-03-21 11:49:39 +1100

    Fixed ssrf demo.

--- demos/server-side/ssrf/client/app.py
+++ demos/server-side/ssrf/client/app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, request, Response, redirect, flash
 
 app = Flask(
     __name__,
-    template_folder='src/templates',
-    static_folder='src/static',
+    template_folder='templates',
+    static_folder='static',
     static_url_path=''
 )
 
@@ -14,12 +14,21 @@ def index():
     if request.method == 'GET':
         return render_template('index.html')
 
+    image_url = request.form.get('url')
+    if not image_url:
+        print('no image')
+        return render_template('index.html', error='No image provided')
+    if not image_url.startswith('http'):
+        print('base64 image')
+        return render_template('index.html', error='We don\'t support base64 images yet')
+
     image_id = uuid.uuid4()
-    image = requests.get(request.form['url'])
+    print(image_id)
+    image = requests.get(image_url)
     if ('html' in image.headers['content-type']):
         return render_template('index.html', error='ERROR: not a valid image!!\n' + image.content.decode())
 
-    with open(f'client/src/static/img/{image_id}.png', 'wb+') as f:
+    with open(f'client/static/img/{image_id}.png', 'wb+') as f:
         f.write(image.content)
 
     return render_template('index.html', file=image_id)
