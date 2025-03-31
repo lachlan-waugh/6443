55f9b7187c1158203b74cc7f9759a3eb8a4fd27e
Lachlan <lachlanwaugh8@gmail.com>
2025-03-31 22:09:33 +1100

    renamed & moved old exam questions.

--- demos/client-side-injection/js-injection/src/templates/template.js
+++ demos/client-side-injection/js-injection/src/templates/template.js
@@ -3,7 +3,6 @@ setTimeout(() => {
     // wait huh how do i use event handlers again
     // idk, but this seems to work fine :shrug:
     const input = '{{ email }}';
-    '; fetch()
     if (/\w+@\w+\.com/.test('{{ email }}')) {
         document.querySelector('#success').innerText = 'ok we will get in contact with you';
         document.querySelector('#success').classList.remove('d-none');
